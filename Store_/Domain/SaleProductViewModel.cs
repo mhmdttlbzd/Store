698125a9b39941ea106df1db5266584724d5528d
mmd <mhmdttlbzd@gmail.com>
2023-06-07 11:19:10 +0430

    complit

---
+++ Store_/Domain/SaleProductViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Domain
+{
+    public class SaleProductViewModel
+    {
+        public string Name { get; private set; }
+        public int ProductId { get; private set; }
+        public int ProductQuantity { get; private set; }
+        public int ProductPrice { get; private set; }
+        public DateTime SaleDate { get;private set; }
+
+        public SaleProductViewModel(string name , int productId,int productQuantity,int productPrice) 
+        {
+            Name = name;
+            ProductId = productId;
+            ProductQuantity = productQuantity;
+            ProductPrice = productPrice;
+            SaleDate = DateTime.Now;
+        }
+        public SaleProductViewModel(string str)
+        {
+            string[] strArry = str.Split('-');
+            Name = strArry[0];
+            ProductId = int.Parse(strArry[1]);
+            ProductQuantity = int.Parse(strArry[2]);
+            ProductPrice = int.Parse(strArry[3]);
+            SaleDate = Convert.ToDateTime(strArry[4]);
+        }
+        public override string ToString()
+        {
+            return Name + "-" + ProductId.ToString() + "-" + ProductQuantity.ToString() + "-" + ProductPrice.ToString() + "-" + SaleDate.ToString();
+        }
+        
+    }
+}
--- Store_/Interface/ProductRepository.cs
+++ Store_/Interface/ProductRepository.cs
@@ -22,20 +22,22 @@ namespace Store.Interface
         {
             try{ CheckProductName(product.Name);}
             catch(Exception ex) { return(ex.Message); }
+            SaveChanges();
             return "ok";
+
         }
         public string GetProductById(int id)
         {
             Product product = products.Single(p => p.ProductId == id);
             return product.Name;
         }
-        List<Product> IProductRepository.GetProductList()
+        public List<Product> GetProductList()
         {
             return products;
         }
         private void CheckProductName(string productName)
         {
-            Regex regex = new Regex(@"^[A-X][a-x]{2}[1-9]{3}_$");
+            Regex regex = new Regex(@"^[A-X][a-x]{2}[0-9]{3}_$");
             if (!regex.IsMatch(productName))
                 throw new ProductNameExeption();
         }
